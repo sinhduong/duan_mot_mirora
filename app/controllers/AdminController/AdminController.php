f7e200e80df670c2eff8418adc7e936f806dfb56
sinhduong <sinhduong1508@gmail.com>
2024-09-23 13:56:29 +0700

    xong admin sản phẩm

--- app/controllers/AdminController/AdminController.php
+++ app/controllers/AdminController/AdminController.php
@@ -2,6 +2,7 @@
 if (isset($_GET['act'])) {
     $act = $_GET['act'];
     switch ($act) {
+        //Danh mục
         case 'adddm':
             if (isset($_POST['adddanhmuc'])) {
                 $name = $_POST['name'];
@@ -40,6 +41,73 @@ if (isset($_GET['act'])) {
                 echo '<script>window.location.href="index.php?act=alldm"</script>';
             }
             break;
+
+     // SẢN PHẨM
+case 'addsp':
+    if (isset($_POST['addsanpham'])) {
+        $name = $_POST['name'];
+        $gia = $_POST['gia'];
+        $gia_giam = $_POST['gia_giam'];
+        $mo_ta = $_POST['mo_ta'];
+
+        // Kiểm tra xem người dùng có tải ảnh lên không
+        if ($_FILES['hinh_anh']['name'] != "") {
+            $hinh_anh = basename($_FILES["hinh_anh"]["name"]);
+            $target_dir = "../../../public/images/";
+            $target_file = $target_dir . $hinh_anh;
+            move_uploaded_file($_FILES["hinh_anh"]["tmp_name"], $target_file);
+        } else {
+            $hinh_anh = "";
+        }
+
+        $iddm = $_POST['iddm'];
+        insert_sanpham($name, $gia, $gia_giam, $mo_ta, $hinh_anh, $iddm);
+        echo '<script>alert("Thêm thành công")</script>';
+        echo '<script>window.location.href= "index.php?act=allsp"</script>';
+    }
+
+    $listdanhmuc = loadall_danhmuc();
+    include "sanpham/addsanpham.php";
+    break;
+
+case 'allsp':
+    include "sanpham/allsanpham.php";
+    break;
+
+case 'editsp':
+    if (isset($_POST['capnhat'])) {
+        $id = $_GET['id_edit'];
+        $name = $_POST['name'];
+        $gia = $_POST['gia'];
+        $gia_giam = $_POST['gia_giam'];
+        $mo_ta = $_POST['mo_ta'];
+
+        if ($_FILES['hinh_anh']['name'] != "") {
+            $hinh_anh = basename($_FILES["hinh_anh"]["name"]);
+            $target_dir = "../../../public/images/";
+            $target_file = $target_dir . $hinh_anh;
+            move_uploaded_file($_FILES["hinh_anh"]["tmp_name"], $target_file);
+        } else {
+            $hinh_anh = ""; // Hoặc giữ hình ảnh cũ
+        }
+
+        $iddm = $_POST['iddm'];
+        update_sanpham($id, $name, $gia, $gia_giam, $mo_ta, $hinh_anh, $iddm);
+        echo '<script>alert("Cập nhật thành công")</script>';
+        echo '<script>window.location.href= "index.php?act=allsp "</script>';
+    } else {
+        include "sanpham/editsanpham.php";
+    }
+    break;
+
+case 'delete':
+    $id = $_GET['iddl']; // Sử dụng đúng tên biến 'iddl'
+    delete_sanpham($id);
+    echo '<script>alert("Xóa thành công")</script>';
+    echo '<script>window.location.href= "index.php?act=allsp "</script>';
+    break;
+
+
 
         default:
             include "layout/home.php";
